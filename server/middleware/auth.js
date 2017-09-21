12c3a8f467bc021b640c3d3f3eb67baa97e97c62
Hack Reactor Student <student@hackreactor.com>
2017-09-21 12:02:14 -0700

    Create session when there is no cookie and pass down to server

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,10 +6,24 @@ module.exports.createSession = (req, res, next) => {
   console.log('**** create sessions');
   session.create().then(resolve => {
     console.log('*** this is resolve:', resolve);
+    var sessionId = resolve.insertId;
+    session.get({ id: sessionId }).then(sessionObj => {
+      //attach to the response
+      console.log('SUCCESS');
+      //res.cookie('sessionId', sessionObj.hash);
+      //session.hash
+      req.session = { 'hash': sessionObj.hash };
+      next();
+    }).catch(sessionReject => {
+      //do something about failure
+      console.log('REJECT');
+      next();
+    });
+    console.log('***this is resolve ID: ', sessionId);
   }).catch(reject => {
     console.log('***This is reject: ', reject);
+    next();
   });
-  next();
 };
 
 /************************************************************/
