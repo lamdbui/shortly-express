a430373508d1862912597d8ded4839c23a12a688
Hack Reactor Student <student@hackreactor.com>
2017-09-21 16:41:03 -0700

    Handle user sessions

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,27 +5,18 @@ const user = models.Users;
 
 module.exports.createSession = (req, res, next) => {
   console.log('**** create sessions');
-  console.log('COOKIE:', req);
   if (req.cookies === undefined || Object.keys(req.cookies).length === 0) {
     session.create().then(resolve => {
-      console.log('*** this is resolve:', resolve);
       var sessionId = resolve.insertId;
       session.get({ id: sessionId }).then(sessionObj => {
-        //attach to the response
-        console.log('SUCCESS');
-        //res.cookie('sessionId', sessionObj.hash);
-        //session.hash
         req.session = { 'hash': sessionObj.hash };
         res.cookie('shortlyid', sessionObj.hash);
         next();
       }).catch(sessionReject => {
         //do something about failure
-        console.log('REJECT');
         next();
       });
-      console.log('***this is resolve ID: ', sessionId);
     }).catch(reject => {
-      console.log('***This is reject: ', reject);
       next();
     });
   } else { // we have a cookie
@@ -40,18 +31,14 @@ module.exports.createSession = (req, res, next) => {
     // sessionObj = { user.username, userId }
     // res.session = sessionObj
     var cookie = req.cookies;
-    console.log('%%% COOKIE:', cookie);
-    console.log('%%% COOKIE HASH:', cookie.shortlyid);
     session.get({'hash': cookie.shortlyid})
       .then(sessionResolve => {
-        console.log('+++ RESOLVE:', sessionResolve);
         // need to check if we actually found any session rows
         if (sessionResolve !== undefined) {
           if (sessionResolve.userId !== null) {
             // let userId = sessionResolve.userId;
             user.get({'id': sessionResolve.userId })
               .then(userResolve => {
-                console.log('^^^ USER_RESOLVE:', userResolve);
                 var sessionObj = {
                   'hash': userResolve.hash,
                   'user': {
@@ -64,9 +51,6 @@ module.exports.createSession = (req, res, next) => {
                 next();
               })
               .catch(userReject => {
-                // res.cookie('shortlyid', sessionResolve.hash);
-                // req.session = { 'hash': sessionResolve.hash };
-                console.log('^^^ USER_REJECT:', userReject);
                 var sessionObj = {
                   'hash': cookie.shortlyid,
                   'user': {
@@ -74,11 +58,8 @@ module.exports.createSession = (req, res, next) => {
                   },
                   'userId': ''
                 };
-                console.log('SESSION_OBJ:', sessionObj);
                 res.cookie('shortlyid', cookie.shortlyid);
                 req.session = sessionObj;
-                console.log('###SESSION:', req.session);
-                console.log('###SESSION TYPE:', typeof req.session);
                 next();
               });
           } else { // when userId is NULL
@@ -94,9 +75,6 @@ module.exports.createSession = (req, res, next) => {
       })
       .catch(sessionReject => {
         // maybe create a new session here as well...
-        // console.log('+++ REJECT:', sessionReject);
-        console.log('+++ REJECT COOKIE:', req.cookies);
-        console.log('--- ', typeof req.cookies);
         res.cookie('shortlyid', req.cookies.shortlyid);
         req.session = { 'hash': req.cookies.shortlyid };
         next();
